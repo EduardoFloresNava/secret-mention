9dfe871fe0433081814c5b352180923f0d7be76c
Jeduan Cornejo <jeduan@gmail.com>
2012-02-06 17:44:37 -0600

    Mensaje enviado

--- ui.js
+++ ui.js
@@ -10,6 +10,9 @@ $(function(){
     $('.sobre').addClass('up')
     setTimeout(function() {
       $('.form').addClass('up');
+      setTimeout(function(){
+        $('.sobre .front').text('Mensaje Enviado');
+      }, 1000)
     }, 1000);
   })
   
