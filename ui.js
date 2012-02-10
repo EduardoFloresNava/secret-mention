67e06fb5c5d864a4c4d5bfaead1eda340a5fd6a6
Jeduan Cornejo <jeduan@gmail.com>
2012-02-10 02:53:30 -0600

    Unas sombras, carnal

--- ui.js
+++ ui.js
@@ -1,9 +1,4 @@
 $(function(){
-  var max_count = 140;
-  $('textarea').on('keyup', function() {
-    count = $(this).val().length;
-    $('span.count').text(max_count - count);
-  });
   
   $('form').on('submit', function(e) {
     e.preventDefault();
